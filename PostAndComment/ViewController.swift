2db90e6d0cde90be4dcecdee2a01b570694bdbe1
Ivan Grga <grga1349@gmail.com>
2019-09-17 21:13:24 +0200

    Initial Commit

---
+++ PostAndComment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PostAndComment
+//
+//  Created by Ivan Grga on 17/09/2019.
+//  Copyright © 2019 Ivan Grga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
